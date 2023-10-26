b9f581aa56713b17eade474f0f99ccaa1f229665
Aaron <Afraz91@wgu.edu>
2023-10-26 09:04:13 -0700

    Updated all files

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const emailField = document.getElementById("email");
-  const confirmEmailField = document.getElementById("confirm-email");
+  const form = document.getElementById("contact-form");
+  const emailField = document.getElementById("email-address");
+  const confirmEmailField = document.getElementById("confirm-email-address");
   const emailMatchMessage = document.getElementById("email-match-message");
 
   // Function to validate email fields
@@ -8,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
       const email = emailField.value.trim();
       const confirmEmail = confirmEmailField.value.trim();
 
-      if (email !== confirmEmail) {
+      if (email!== confirmEmail) {
           emailMatchMessage.textContent = "Email addresses do not match.";
       } else {
           emailMatchMessage.textContent = "Email addresses match!";
@@ -18,4 +19,17 @@ document.addEventListener("DOMContentLoaded", function () {
   // Add event listener for email field and confirm email field
   emailField.addEventListener("input", validateEmails);
   confirmEmailField.addEventListener("input", validateEmails);
+
+  // Add event listener for form submission
+  form.addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      // Validate email fields
+      validateEmails();
+
+      // If email fields are valid, submit the form
+      if (emailMatchMessage.textContent === "Email addresses match!") {
+          form.submit();
+      }
+  });
 });
